f1a577e754575af3212577043476641aabd28ccd
Janice Park <pjm605@gmail.com>
2016-08-05 12:58:18 -0400

    middle point

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,14 @@ module.exports = function makeRouterWithSockets (io) {
   // a reusable function
   function respondWithAllTweets (req, res, next){
     
-    client.query('SELECT * FROM tweets', function (err, result) {
+    client.query('SELECT * from users Join tweets on tweets.userid = users.id', function (err, result) {
   if (err) return next(err); // pass errors to Express
   var tweets = result.rows;
+  console.log(tweets)
   res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
 });
-    // var allTheTweets = tweetBank.list();
-    // res.render('index', {
-    //   title: 'Twitter.js',
-    //   tweets: allTheTweets,
-    //   showForm: true
-    // });
-  }
+
+ }
 
   // here we basically treet the root view and tweets view as identical
   router.get('/', respondWithAllTweets);
@@ -41,20 +37,12 @@ module.exports = function makeRouterWithSockets (io) {
 
     });
 
-
-    // var tweetsForName = tweetBank.find({ name: req.params.username });
-    // res.render('index', {
-    //   title: 'Twitter.js',
-    //   tweets: tweetsForName,
-    //   showForm: true,
-    //   username: req.params.username
-    // });
   });
 
   // single-tweet page
   router.get('/tweets/:id', function(req, res, next){
 
-    client.query('SELECT * FROM tweets WHERE ID = $1', [req.params.id], function (err, result)  {
+    client.query('SELECT tweets.content, users.name, users.pictureurl FROM tweets JOIN users ON tweets.userid=users.id WHERE tweets.id = $1', [req.params.id], function (err, result)  {
       if (err) return next(err);
       res.render('index', {
       title: 'Twitter.js',
@@ -63,43 +51,56 @@ module.exports = function makeRouterWithSockets (io) {
 
     })
 
-    // var tweetsWithThatId = tweetBank.find({ id: Number(req.params.id) });
-    // res.render('index', {
-    //   title: 'Twitter.js',
-    //   tweets: tweetsWithThatId // an array of only one element ;-)
-    // });
   });
 
   // create a new tweet
-  router.post('/tweets', function(req, res, next){
+  router.post('/tweets', function(req, res, next) {
     client.query('SELECT id FROM users WHERE name = $1', [req.body.name], function (err, result) {
       if(err) return next (err);
       
       //console.log(result.rows)
       if(result.rows.length === 0) {
+        client.query('INSERT INTO users (name) VALUES ($1)', [req.body.name], function (err, result) {
+          if(err) return next(err);
+        });
+      client.query('SELECT id FROM users WHERE name=$1', [req.body.name], function (err, result) {
+          if (err) return next(err);
+
+          client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2)', [result.rows, req.body.content], function (err, result) {
+          if (err) return next(err);
+          res.redirect('/');
+        });
+
+        });
         
       } 
       else {
-        var id = result.rows[0]['id']
-        client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2)', [id, req.body.content], function (err, result) {
-        if(err) return next (err);
-        //io.sockets.emit('new_tweet', result)
-         res.redirect('/');
-      })
+        var id = result.rows[0]['id'];
+
+        client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2)', [id, req.body.content], 
+          function (err, result) {if(err) return next(err); });
+        client.query('INSERT INTO tags (tag) VALUES ($1)', [req.body.hashtag], function (err, result) {
+           if (err) return next(err);
+        });
+        client.query('SELECT id FROM tags WHERE tag = $1', [req.body.hashtag], function (err, result) {
+          if (err) return next(err);
+          else {
+            var tagID = result.rows[0]['id'];
+            client.query('SELECT id FROM tweets WHERE content=$1', [req.body.content], function (err, result) {
+              if (err) return next (err);
+              else {
+                var tweetID = result.rows[0]['id'];
+                client.query('INSERT INTO tags_tweets (tag_id, tweet_id) VALUES ($1, $2)', [tagID, tweetID], function (err, result) {if (err) 
+                return next(err);
+                else res.redirect('/');});
+              }
+            })}
+        });
       }
 
- 
-    })
 
-    // var newTweet = tweetBank.add(req.body.name, req.body.content);
-    // io.sockets.emit('new_tweet', newTweet);
-    // res.redirect('/');
-  });
+        })});
 
-  // // replaced this hard-coded route with general static routing in app.js
-  // router.get('/stylesheets/style.css', function(req, res, next){
-  //   res.sendFile('/stylesheets/style.css', { root: __dirname + '/../public/' });
-  // });
 
   return router;
 }
